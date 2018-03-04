c94bb997cdd4ff845edb492b99f6abd5e6890efa
Matyáš Bach <matyasbach@plus4u.net>
2018-03-04 02:08:33 +0100

    Default Header

--- components/header.js
+++ components/header.js
@@ -1,77 +1,175 @@
+import { Component } from 'react'
 import Link from 'next/link'
 
-const Menu = () => (
-  <div className='menu'>
-    <ul>
-      <li>
-        <Link href="/portfolio">
-          <a>Portfolio</a>
-        </Link>
-      </li>
-      <li>
-        <Link as="/vystavy" href="/exhibitions">
-          <a>Výstavy</a>
-        </Link>
-      </li>
-      <li>
-        <Link as="/o-mne" href="/about">
-          <a>O mně</a>
-        </Link>
-      </li>
-    </ul>
-    <style jsx>{`
-      ul {
-        float: right;
-        margin: 0;
-      }
+class MobileMenu extends React.Component {
+  state = {
+    visible: false
+  }  
 
+  toggle = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  renderMenu() {
+    return (
+      <ul>
+        <li>
+          <Link href="/portfolio">
+            <a>Portfolio</a>
+          </Link>
+        </li>
+        <li>
+          <Link as="/vystavy" href="/exhibitions">
+            <a>Výstavy</a>
+          </Link>
+        </li>
+        <li>
+          <Link as="/o-mne" href="/about">
+            <a>O mně</a>
+          </Link>
+        </li>
+        <style jsx>{`
+          ul {
+            position: absolute;
+            top: 2.5em;
+            left: 0;
+            width: 100vw;
+            background: #fff;
+            list-style: none;
+          }
+          
+          li {
+            text-align: center;
+            padding: 2em;
+          }
+          
+          a {        
+            padding: 0 0.5em;
+            text-decoration: none;
+            color: #444;
+            font-size: 150%;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+          }
+        `}</style>
+      </ul>
+    )
+  }
+
+  render() {
+    const renderMenu = this.state.visible ? this.renderMenu() : ''
+
+    return (
+      <div>
+        <button type="button" onClick={this.toggle}>Menu</button>
+        {renderMenu}
+        <style jsx>{`
+          button {
+            padding: 0 0.5em;
+            text-decoration: none;
+            color: #444;
+            font-size: 150%;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+            border: none;
+            outline: none;
+            background: none;
+
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+const FullMenu = () => (
+  <ul>
+    <li>
+      <Link href="/portfolio">
+        <a>Portfolio</a>
+      </Link>
+    </li>
+    <li>
+      <Link as="/vystavy" href="/exhibitions">
+        <a>Výstavy</a>
+      </Link>
+    </li>
+    <li>
+      <Link as="/o-mne" href="/about">
+        <a>O mně</a>
+      </Link>
+    </li>
+    <style jsx>{`      
+      ul {
+        margin: 0.5em 2em;
+        /*margin-right: 20vh;*/
+        list-style: none;
+        align-self: flex-end;
+      }
+      
       li {
-        margin: 0.1em;
         display: inline-block;
       }
       
       a {
+        padding: 0 0.5em;
         text-decoration: none;
         color: #444;
-        padding: 2em 1.5em 2em;
-        background: green;
+        font-size: 150%;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        border-right: 1px solid gray;
+      }
+      li:last-child a {
+        border: none;
       }
     `}</style>
-  </div>
+  </ul>
 )
 
-const FloatClearer = () => (
-  <div>
-    <style jsx>{`
-      div {
-        clear: both;
-      }
-    `}</style>
-  </div>
-)
+class Menu extends Component {
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({ mobile: window.innerWidth < 750 });
+  }
+
+  state = {
+    mobile: true,
+  }
+
+  render() {
+    if(this.state.mobile)
+      return <MobileMenu />
+    else
+      return <FullMenu />
+  }
+}
 
 const Header = () => (
-  <div className="header">
+  <div>
     <Link href="/">
       <a>Ivana Mrázková</a>
     </Link>
     <Menu />
-    <FloatClearer />
     <style jsx>{`
+      div {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 2px solid #222;
+      }
       a {
         text-decoration: none;
-        color: #444;
-        float: left;
+        color: #301030;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
         font-size: 200%;
-        font-family: 'Lovelo';
-        display: inline-block;
-        height: 100%;
-      }
-      .header {
-        max-width: 800px;
-        margin: auto;
-        background: #eee;
-        padding: 1em;
+        margin-left: 2em;
       }
     `}</style>
   </div>

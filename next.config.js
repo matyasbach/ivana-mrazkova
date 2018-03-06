7efb3e8ec40772569a33f2ff0879a231b616e262
Matyáš Bach <matyasbach@plus4u.net>
2018-03-06 23:45:43 +0100

    Removed node server (express) dependency

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/o-mne': { page: '/o-mne' },
+      '/portfolio': { page: '/portfolio' },
+      '/vystavy': { page: '/vystavy' }
+    }
+  }
+}

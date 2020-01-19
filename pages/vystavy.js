dbbd5183fd1fcf59dada1a9f8435d420b6437980
Matyáš Bach <matyas.bach@plus4u.net>
2020-01-19 20:16:22 +0100

    Exhibitions updated and taken from json datasource

--- pages/vystavy.js
+++ pages/vystavy.js
@@ -1,86 +1,46 @@
 import Layout from '../components/layout'
+import allExhibitions from '../data/exhibitions.json'
 
-export default () => (
-  <Layout>
-    <h2>Aktuální</h2>
+const postersPath = "../static/posters/"
 
+const groupedExhibitions = (() => {
+  const planned = []
+  const current = []
+  const pastGroup = [];
+  const pastSolo = []
+
+  const today = new Date();
+  allExhibitions.forEach(exhibition => {
+    const startDate = new Date(exhibition.startDate);
+    const endDate = new Date(exhibition.endDate);
+    
+    if (startDate > today) planned.push(exhibition)
+    else if (endDate > today) current.push(exhibition)
+    else if (exhibition.type == "group") pastGroup.push(exhibition)
+    else pastSolo.push(exhibition)
+  });
+
+  return { planned, current, pastSolo, pastGroup };
+})();
+
+const FeaturedExhibitionsGroup = (title, exhibitions) => (
+  exhibitions.length > 0 && (
+    <div>
+      <h2>{title}</h2>
+      {exhibitions.map(FeaturedExhibition)}
+    </div>
+  )
+)
+
+const FeaturedExhibition = ({ title, place, detailedPlace, startDate, endDate, link, linkTitle, poster, posterTitle }) => (
+  <div key={title}>
     <p>
-      Na Hladině<br />
-      Galerie FlexUp, Lípová 10, Praha 2<br />
-      19. 12. 2019 - 30. 1. 2020<br /><br />
-      <a href="https://www.facebook.com/events/460683267926157/" title="Výstava Na Hladině - událost na facebooku">
-        https://www.facebook.com/events/460683267926157/
-      </a>
+      {title}<br />
+      {detailedPlace || place}<br />
+      {new Date(startDate).toLocaleDateString()} - {new Date(endDate).toLocaleDateString()}<br /><br />
+      {link && <a href={link} title={linkTitle}>{link}</a>}
     </p>
-    <img src="../static/posters/na-hladine-flexup-praha-2019.jpg" title="Pozvánka na výstavu Na Hladině" />
-
-    <p>
-      V kapce vody<br />
-      Galerie na Miladě, Praha<br />
-      28. 11. 2019 - 17. 1. 2020<br /><br />
-      <a href="https://www.praha7.cz/ivana-mrazkova-v-kapce-vody/" title="Praha 7 - Galerie na Miladě">
-        https://www.praha7.cz/ivana-mrazkova-v-kapce-vody/
-      </a>
-    </p>
-    <img src="../static/posters/v-kapce-vody-galerie-na-milade-praha-2019.png" title="Pozvánka na výstavu V kapce vody" />
-
-    <p>
-      Dialogy<br />
-      Galerie Kooperativy, Praha<br />
-      25. 10. - 2. 2. 2020<br /><br />
-      <a href="https://www.koop.cz/pojistovna-kooperativa/galerie-kooperativy" title="Galerie Kooperativy">
-        https://www.koop.cz/pojistovna-kooperativa/galerie-kooperativy
-      </a>
-    </p>
-    <img src="../static/posters/dialogy-kooperativa-praha-2019.png" alt="Pozvánka na výstavu Dialogy 2019"></img>
-
-    <p>
-      Malba III<br />
-      Galerie Millenium, Praha<br />
-      20. 11. 2019 - 5. 1. 2020<br /><br />
-      <a href="http://www.gallerymillennium.cz/soucasna_vystava.html" title="Galerie Millenium">
-        http://www.gallerymillennium.cz/soucasna_vystava.html
-      </a>
-    </p>
-    <img src="../static/posters/malba-III-praha-2019.jpg" alt="Plakát k výstavě Malba III"></img>
-
-    <h2>Samostatné výstavy</h2>
-    <table>
-      <tbody>
-        <tr><th>            2019</th><td>Po proudu, Art Café, Liberec</td></tr>
-        <tr><th>            2019</th><td>Lesem i vodou, Komunitní&nbsp;centrum&nbsp;Háta, Ledeč&nbsp;nad&nbsp;Sázavou</td></tr>
-        <tr><th>            2019</th><td>Putování krajinou, Ministerstvo&nbsp;zemědělství, Praha</td></tr>
-        <tr><th>            2018</th><td>Krajina, JazyGo, Praha</td></tr>
-        <tr><th>            2018</th><td>Tam a zpět, Galerie&nbsp;Thambos, Plzeň</td></tr>
-        <tr><th>            2018</th><td>Náruč lesa, Ministerstvo&nbsp;zemědělství, Praha</td></tr>
-        <tr><th>            2017</th><td>Lesem Proudem, Dobrá&nbsp;Trafika, Praha</td></tr>
-      </tbody>
-    </table>
-
-    <h2>Skupinové výstavy</h2>
-    <table>
-      <tbody>
-        <tr><th>            2019</th><td>Spectators Disease, Polansky&nbsp;Gallery/Holešovická&nbsp;šachta, Praha</td></tr>
-        <tr><th>            2019</th><td>Generace, Galerie&nbsp;Kooperativy, Praha</td></tr>
-        <tr><th>            2019</th><td>Pohled do krajiny, Galerie&nbsp;České&nbsp;spořitelny, Praha</td></tr>
-        <tr><th>            2018</th><td>Mánes Mánesu, Galerie&nbsp;Špejchar, Chomutov</td></tr>
-        <tr><th rowSpan="2">2018</th><td>Rozbřesk - současná krajinomalba</td></tr>
-        <tr><td>                         Východočeská galerie v&nbsp;Pardubicích</td></tr>
-        <tr><th>            2018</th><td>Art Prague, Clam&#8209;Gallasův palác, Praha</td></tr>
-        <tr><th>            2018</th><td>Členská výstava S.V.U.&nbsp;Mánes, Galerie&nbsp;Diamant</td></tr>
-        <tr><th>            2017</th><td>Světlo v obraze: český&nbsp;impresionismus, Jízdárna&nbsp;Pražského&nbsp;hradu</td></tr>
-        <tr><th>            2017</th><td>ATRIBUT, Galerie&nbsp;J°CEJKA, Praha</td></tr>
-        <tr><th>            2017</th><td>Design piknik, Počátky</td></tr>
-        <tr><th rowSpan="2">2017</th><td>Malíři současnosti - Sochaři minulosti, Galerie&nbsp;Diamant</td></tr>
-        <tr><td>                         Výstava ke 130. výročí založení spolku S.V.U.&nbsp;Mánes</td></tr>
-        <tr><th>            2017</th><td>Krajina 2017, Nová&nbsp;galerie</td></tr>
-        <tr><th>            2016</th><td>Pokoje, Ateliérová výstava Malby&nbsp;3</td></tr>
-        <tr><th rowSpan="2">2015</th><td>Těsně vedle, Ateliérová výstava Malby&nbsp;3</td></tr>
-        <tr><td>                         Galerie Josefa Lieslera v&nbsp;Kadani</td></tr>
-        <tr><th>            2015</th><td>Cafe Decada v&nbsp;Praze společně s&nbsp;Terezou Zichovou</td></tr>
-        <tr><th>            2015</th><td>PETROHRADSKÁ Opening season #159!</td></tr>
-      </tbody>
-    </table>
+    {poster && <img src={postersPath + poster} title={posterTitle} />}
     <style jsx>{`
       a {
         color: rgb(70,70,70);
@@ -94,16 +54,6 @@ export default () => (
         display: flex;
         margin: 50px auto;
       }
-      table {
-        width: 100%;
-      }
-      th {
-        min-width: 60px;
-        width: 5%;
-        vertical-align: top;
-        text-align: left;
-        font-weight: normal;
-      }
 
       @media only screen and (min-width: 750px) {
         img {
@@ -111,5 +61,47 @@ export default () => (
         }
       }
     `}</style>
+  </div>
+)
+
+const PastExhibitionsGroup = (title, exhibitions) => (
+  exhibitions.length > 0 && (
+    <div>
+      <h2>{title}</h2>
+      <table>
+        <tbody>{exhibitions.map(PastExhibition)}</tbody>
+      </table>
+      <style jsx>{`
+        table { width: 100%; }
+      `}</style>
+    </div>
+  )
+)
+
+const PastExhibition = ({ title, place, startDate, note }) => [
+  <tr key={title}>
+    <th>{new Date(startDate).getFullYear()}</th>
+    <td>{withNonBreakingCharacters(title)}{place && `, ${withNonBreakingCharacters(place)}`}</td>
+    <style jsx>{`
+      th {
+        min-width: 60px;
+        width: 5%;
+        vertical-align: top;
+        text-align: left;
+        font-weight: normal;
+      }
+    `}</style>
+  </tr>,
+  note && <tr key={title + "-note"}><th></th><td>{note}</td></tr>
+]
+
+const withNonBreakingCharacters = text => text.replace(" ", "\u00A0").replace("-", "\u2011");
+
+export default () => (
+  <Layout>
+    {FeaturedExhibitionsGroup("Aktuální", groupedExhibitions.current)}
+    {FeaturedExhibitionsGroup("Plánované", groupedExhibitions.planned)}
+    {PastExhibitionsGroup("Samostatné výstavy", groupedExhibitions.pastSolo)}
+    {PastExhibitionsGroup("Skupinové výstavy", groupedExhibitions.pastGroup)}
   </Layout>
 )
